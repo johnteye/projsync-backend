f7a89f30687ec4642410886fc593aa65a42db9cd
John Teye Doku <86769906+johnteye@users.noreply.github.com>
2025-05-21 16:52:53 +0000

    Initial commit

---
+++ src/main/java/com/example/projsync_backend/ProjsyncBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.projsync_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjsyncBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjsyncBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projsync_backend/ProjsyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projsync_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjsyncBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
